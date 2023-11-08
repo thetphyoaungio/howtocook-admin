492eec67e119d30d892d4db1930b2f28446dea12
thetphyoaungio <kyawaung0003@gmail.com>
2023-11-08 14:12:31 +0630

    .

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,17 @@
 export const environment = {
   production: true,
 
-  APIEP:'https://13.250.34.122/api/v1/adminUser',
-  FOLLUSERAPIEP:'https://13.250.34.122/api/v1/follow',
+  APIEP:'http://13.250.53.111',//https://howtocook.click',//https://api.musiclink.click
+  APIPATH:'api/v1',
+  ADMUSER:'adminUser',
+  MOBUSER:'mobileUser',
+  POST:'post',
+  COMNT:'comment',
+  CATEG:'category',
+  TIP:'tip',
+  NOTI:'notification',
+  FEDBCK:'feedback',
+  ADS:'ads',
+  APVU:'app_version',
+  FQS:'FAQ',
 };
